ed15c41e35245310ea3bc0766048129d62298d59
Deniz <167799743+T-vulpes@users.noreply.github.com>
2024-09-30 16:32:38 +0300

    Mainn.java

--- Inverted_Triangle/src/Mainn.java
+++ Inverted_Triangle/src/Mainn.java
@@ -8,10 +8,8 @@ public class Mainn{
         System.out.print("Choose triangle type (1 for Inverted, 2 for Normal): ");
         int choice = scanner.nextInt();
 
-        // Kullanıcının seçimine göre üçgen çiziyoruz
         switch (choice) {
             case 1:
-                // Ters Üçgen (Inverted Triangle)
                 System.out.println("Inverted Triangle:");
                 for (int i = rows; i >= 1; i--) {
                     for (int j = 1; j <= rows - i; j++) {
@@ -25,7 +23,6 @@ public class Mainn{
                 break;
 
             case 2:
-                // Düz Üçgen (Normal Triangle)
                 System.out.println("Normal Triangle:");
                 for (int i = 1; i <= rows; i++) {
                     for (int j = 1; j <= rows - i; j++) {
